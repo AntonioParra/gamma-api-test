e1b0a20614ae41e54a8c241bb5bf2755bd7dbef6
Antonio Parra <antparu@gmail.com>
2024-07-14 14:34:09 +0200

    temporal email test

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const { createServer } = require('node:http');
+const { createServer } = require('node:http')
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello World');
+  fetch('http://rp-internal:80/internal/email/sendEmail', {
+    method: 'POST',
+    body: JSON.stringify({
+      to: '',
+      subject: 'asunto asunto asunto',
+      body: 'mensaje mensaje mensaje'
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(data=>data.text()).then(data => {
+    res.statusCode = 200
+    res.end(JSON.stringify(data))
+  }).catch(error => {
+    res.statusCode = 500
+    res.end(JSON.stringify(error.toString()))
+  });
 });
 
 server.listen(port, () => {
